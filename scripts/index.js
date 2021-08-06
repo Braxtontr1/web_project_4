33ec6d03d502ee5ad0fffdd9a1a4c71413875536
Braxton Trone <braxtontrone@gmail.com>
2021-08-05 23:34:50 -0500

    fixed bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 const modalElement = document.querySelector('.modal');
-const modalBttnElement = document.querySelector('#modal-open-button');
-const modalBttnClose = document.querySelector('#modal-close');
-const modalFormElement = document.querySelector('#modal-edit-form');
+const modalBttnElement = document.querySelector('.profile__button');
+const modalBttnClose = document.querySelector('.modal__close-button');
+const modalFormElement = document.querySelector('.form');
+const profileName = document.querySelector('.profile__name');
+const profileTitle = document.querySelector('.profile__about-me');
 const modalNameInput = document.querySelector('#modal-name-input');
 const modaltitleInput = document.querySelector('#modal-title-input');
-const profileName = document.querySelector('#profile-name');
-const profileTitle = document.querySelector('#profile-title');
 
 function openModal() {
     modalElement.classList.add('modal_open');
+    modalNameInput.value = profileName.textContent;
+    modaltitleInput.value = profileTitle.textContent;
 }
 
 function closeModal() {
     modalElement.classList.remove('modal_open');
 }
 
-modalBttnElement.addEventListener('click', openModal);
-
-modalBttnClose.addEventListener ('click', closeModal);
-
-modalFormElement.addEventListener('submit', (e) => {
+function saveModal(e) {
     e.preventDefault();
     profileName.textContent = modalNameInput.value;
     profileTitle.textContent = modaltitleInput.value;
     closeModal();
-});
+}
+
+modalBttnElement.addEventListener('click', openModal);
+
+modalBttnClose.addEventListener('click', closeModal);
+
+modalFormElement.addEventListener('submit', saveModal);
