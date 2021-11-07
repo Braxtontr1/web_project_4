db676216b34dde6adee33493167c379f8a1cd3d4
Braxton Trone <braxtontrone@gmail.com>
2021-11-07 01:26:56 -0500

    fixed submit button resetting

--- scripts/index.js
+++ scripts/index.js
@@ -31,7 +31,6 @@ const initialCards = [{
 const destinationTemplate = document.querySelector('#destination-template').content.querySelector('.destination');
 const destinations = document.querySelector('.destinations');
 
-const page = document.querySelector('.page');
 const editModal = document.querySelector('.modal_type_edit-profile');
 const editForm = editModal.querySelector('.form');
 const addModal = document.querySelector('.modal_type_add-card');
